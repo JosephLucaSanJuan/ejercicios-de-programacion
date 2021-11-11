56dafbbec5119bb08136870162d1fc52eaa6be0e
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2021-11-11 09:29:24 +0100

    actualizando repositorio de programacion

--- Tema04/Tema04Ejercicio15.java
+++ Tema04/Tema04Ejercicio15.java
@@ -8,21 +8,21 @@
 public class Tema04Ejercicio15 {
 	
 	public static void main (String[] args) {
-		char z;
-    double direccion;
+		String z;
+    int direccion;
     
     System.out.print("Introduce el caracter que quieres usar para construir la piramide: ");
-    double z = Double.parseDouble(System.console.().readLine()).charAt(0);
+    z = System.console().readLine();
     
     System.out.print("Introduce la dirección en la que quieras que apunte el vértice de la piramide: ");
     System.out.println("\n1--> Arriba \n2--> Derecha \n3-->Abajo \n4-->Izquierda");
     System.out.print("Introduce tu respuesta: ");
-    direccion = Double.parseDouble(System.console().readLine());
+    direccion = Integer.parseInt(System.console().readLine());
     
     switch((int)direccion){
       case 1:
       System.out.println("\n " + z + " \n " + z + z + z + " ");
-      System.out.println(" " + z + z + z + z + z + z + z " ");
+      System.out.println(" " + z + z + z + z + z + z + z + " ");
       System.out.println(" " + z + z + z + z + z + z);
       System.out.print(" " + z + z + z + z + z);
       System.out.print(" " + z + z + z + "\n");
@@ -38,18 +38,22 @@ public class Tema04Ejercicio15 {
       
       case 3:
       System.out.println("\n" + z + z + z + z + z + z);
-      System.out.println(" " + z + z + z "\n");
-      System.out.println(" "+ x + x + x + x + x + x + x + " ");
-      System.out.print(" " + x + x + x + x + x + " ");
-      System.out.print(" " + x + x + x + "  " + "\n " + x + "\n");
+      System.out.println(" " + z + z + z + "\n");
+      System.out.println(" "+ z + z + z + z + z + z + z + " ");
+      System.out.print(" " + z + z + z + z + z + " ");
+      System.out.print(" " + z + z + z + "  " + "\n " + z + "\n");
       break;
       
       case 4:
-      System.out.println("\n    " + x);
-      System.out.println(" " + x + " " + x);
-      System.out.println(x + " " + x + " " + x);
-      System.out.println(" " + x + " " + x);
-      System.out.println(" " + x);
+      System.out.println("\n    " + z);
+      System.out.println(" " + z + " " + z);
+      System.out.println(z + " " + z + " " + z);
+      System.out.println(" " + z + " " + z);
+      System.out.println(" " + z);
+      break;
+      
+      default:
+      System.out.println("Numero erróneo");
       break;
     }
 	}
